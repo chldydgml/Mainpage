4f46b539604e0c874d8e33f0987c64bded00adb5
zzaoo007 <jeongho9805@naver.com>
2021-11-23 23:14:33 +0900

    FBLogin

--- app/src/main/java/com/example/mainpage/Fragment_login.java
+++ app/src/main/java/com/example/mainpage/Fragment_login.java
@@ -1,15 +1,30 @@
 package com.example.mainpage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Fragment_login extends Fragment {
+    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증 처리
+    private DatabaseReference mDatabaseReference; //실시간 데이터 베이스
+    private EditText mEtEmail, mEetPwd;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,7 +33,45 @@ public class Fragment_login extends Fragment {
         View v = inflater.inflate(R.layout.fragment_login, container, false);
 
         return v;
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("FBLoginLast");
+
+        mEtEmail = v.findViewById(R.id.value_id);
+        mEetPwd= v.findViewById(R.id.value_pw);
+
+        Button btn_login = v.findViewById(R.id.button_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 로그인 요청
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEetPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //로그인 성공
+                            Intent intent = new Intent(getActivity(), MainActivity.class);
+                            startActivity(intent);
+                           // finish(); // 현재 액티비티 파괴
+                        }else{
+                            Toast.makeText(getActivity(),"로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        Button btn_register = v.findViewById(R.id.button_signin);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
