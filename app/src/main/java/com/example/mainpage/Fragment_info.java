e47a009551962c23b13030518e69d31f7a0f5e3a
chldydgml <84460998+chldydgml@users.noreply.github.com>
2021-11-25 11:08:10 +0900

    filter drawer

--- app/src/main/java/com/example/mainpage/Fragment_info.java
+++ app/src/main/java/com/example/mainpage/Fragment_info.java
@@ -2,20 +2,19 @@ package com.example.mainpage;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment_filter extends Fragment {
+public class Fragment_info extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_filter, container, false);
+        View v = inflater.inflate(R.layout.fragment_info, container, false);
 
         return v;
     }
--- app/src/main/java/com/example/mainpage/MainActivity.java
+++ app/src/main/java/com/example/mainpage/MainActivity.java
@@ -2,17 +2,36 @@ package com.example.mainpage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private View drawerView;
+    ImageView btn_filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn_filter = findViewById(R.id.icon_filter1);
+        mDrawer = findViewById(R.id.drawer_layout);
+        drawerView = findViewById(R.id.drawerView2);
+        mDrawer.setDrawerListener(listener);
+
+
+        btn_filter.setOnClickListener(v -> mDrawer.openDrawer(drawerView));
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment_home()).commitAllowingStateLoss();
@@ -28,12 +47,12 @@ public class MainActivity extends AppCompatActivity {
                             getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment_search()).commitAllowingStateLoss();
                             break;
 
-                        case R.id.tab_login:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment_login()).commitAllowingStateLoss();
+                        case R.id.tab_info:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment_info()).commitAllowingStateLoss();
                             break;
 
-                        case R.id.tab_filter:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment_filter()).commitAllowingStateLoss();
+                        case R.id.tab_login:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container, new Fragment_login()).commitAllowingStateLoss();
                             break;
                     }
                     return true;
@@ -42,4 +61,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull @NotNull View drawerView, float slideOffset) { }
+        @Override
+        public void onDrawerOpened(@NonNull @NotNull View drawerView) { }
+        @Override
+        public void onDrawerClosed(@NonNull @NotNull View drawerView) { }
+        @Override
+        public void onDrawerStateChanged(int newState) { }
+    };
 }
